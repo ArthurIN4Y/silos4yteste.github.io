307b55c86e89bc6c6ac9d7378b5c51d7a104b942
Arthur Prasniski IN4Y <126330487+ArthurIN4Y@users.noreply.github.com>
2023-03-10 16:36:50 -0300

    update font style grafico

--- src/components/date-chart/index.tsx
+++ src/components/date-chart/index.tsx
@@ -1,13 +1,15 @@
+import { Box } from "@mui/material";
 import { useState } from "react";
 import { RadialChart } from "react-vis";
+import { LabelInter } from "../../utils/typography";
+import * as s from "./styles";
 
 export const DateChart = () => {
   const [data, setData] = useState([
     { angle: 4, value: "45%", color: "#DFBD2C" },
-    { angle: 1, value: "5%",color: "#FFE160" },
-    { angle: 3, value: "30%",color: "#FFEB98" },
-    { angle: 2, value: "20%",color: "#E4E4E4" },
-    
+    { angle: 1, value: "5%", color: "#FFE160" },
+    { angle: 3, value: "30%", color: "#FFEB98" },
+    { angle: 2, value: "20%", color: "#E4E4E4" },
   ]);
 
   return (
@@ -19,17 +21,19 @@ export const DateChart = () => {
 
 function Chart({ data }: any) {
   return (
-    <RadialChart
-      colorType="literal"
-      innerRadius={50}
-      radius={75}
-      data={data}
-      color={(d: any) => d.color}
-      width={160}
-      height={160}
-      animation={"gentle"}
-      showLabels={true}
-      getLabel={(d: any) => d.value}
-    />
+    <Box sx={s.style}>
+      <RadialChart
+        colorType="literal"
+        innerRadius={50}
+        radius={75}
+        data={data}
+        color={(d: any) => d.color}
+        width={160}
+        height={160}
+        animation={"gentle"}
+        showLabels={true}
+        getLabel={(d: any) => d.value}
+      />
+    </Box>
   );
 }
---
+++ src/components/date-chart/styles.ts
@@ -0,0 +1,7 @@
+export const style ={
+    '.rv-xy-plot__series--label-text': {
+        fontFamily: 'Inter',
+        fontSize: '12px',
+        fontWeight: '500',
+    }
+}
